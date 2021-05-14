aedc6f0f8de63ebdcb8588bd844163d20ce11295
Nick Kijak <nkijak@gmail.com>
2021-05-14 16:02:14 +0000

    module/refactor of worker code
    added code zip expansion

--- service/worker.rb
+++ service/worker.rb
@@ -3,33 +3,47 @@ require 'zlib'
 require 'rubygems/package'
 require 'asciidoctor'
 
-def render(book_zip, code_zip)
-    Dir.mktmpdir do |book_dir| 
-        puts book_dir
-        Zlib::GzipReader.open(book_zip) do |bz|
+module Binder
+    def expand_archive(dir, archive)
+        Zlib::GzipReader.open(archive) do |bz|
             Gem::Package::TarReader.new(bz) do |tar|
                 tar.each do |entry|
                     if entry.file?
-                        parent = "#{book_dir}/#{File.dirname(entry.full_name)}"
-                        target = "#{book_dir}/#{entry.full_name}"
-                        puts "mkdir_p #{parent}"
+                        parent = "#{dir}/#{File.dirname(entry.full_name)}"
+                        target = "#{dir}/#{entry.full_name}"
                         FileUtils.mkdir_p(parent)    
                         File.open(target, 'wb') { |f| f.write(entry.read) }
                     end
-                    File.chmod(entry.header.mode, target)
+                    puts entry.header.mode, target
+                    File.chmod(entry.header.mode, target) if entry.header.mode && target
                 end
             end
         end
-        Dir.mktmpdir do |output_dir|
-            Asciidoctor.convert_file(
-                "#{book_dir}/dist/raw.adoc", 
-                to_file: true, 
-                safe: :unsafe,
-                to_dir: output_dir
-            )
-            puts output_dir
+    end
+
+    def render(book_zip, code_zip)
+        Dir.mktmpdir do |temp_dir| 
+            {book: book_zip, code: code_zip}.each do |prefix, archive|
+                dir = "#{temp_dir}/#{prefix}" 
+                puts dir, archive
+                expand_archive dir, archive
+            end
+            Dir.mktmpdir do |output_dir|
+                Asciidoctor.convert_file(
+                    "#{temp_dir}/book/dist/raw.adoc",  #TODO how to find
+                    to_file: true, 
+                    safe: :unsafe,
+                    to_dir: output_dir
+                )
+                yield output_dir
+            end
         end
     end
+
+    module_function(:render)
+    module_function(:expand_archive)
 end
 
-render('tests/resources/book.tar.gz', 'tests/resources/code.tar.gz')
+Binder::render('tests/resources/book.tar.gz', 'tests/resources/code.tar.gz') do |outdir| 
+    puts(outdir) 
+end
